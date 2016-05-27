9dfd76d8dc831e67a2911a85c349f7d036b99971
James Meyers <James.Meyers919@gmail.com>
2016-05-27 09:26:07 -0700

    replaced all with ftph

--- maze-gen/lib/mixins/extend-state.js
+++
@@ -1,9 +0,0 @@
-import _ from "lodash";
-
-const extendStateMixin = {
-    extendState(...sources) {
-        this.setState(_.extend({}, this.state, ...sources));
-    }
-};
-
-export default extendStateMixin;
